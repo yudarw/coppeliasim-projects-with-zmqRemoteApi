6207c910c24aa9f1183e3dc6a2910a4f1f60dba8
yudarw <yudarismawahyudi@gmail.com>
2024-10-19 23:56:12 +0700

    added homogeneous transformation matrix

--- 6_UR10/UR10_demo.py
+++ 6_UR10/UR10_demo.py
@@ -1,7 +1,9 @@
 from coppeliasim_zmqremoteapi_client import *
 import math
 import time
-from interpolation import linear_interpolation
+from interpolation import linear_interpolation, joint_interpolation
+from homogeneous_transform import *
+import numpy as np
 
 client = RemoteAPIClient()
 sim = client.require('sim')
@@ -20,6 +22,8 @@ class UR10:
         self.simTip = self.sim.getObject(f'/{robotName}/ikTip')
         self.simTarget = self.sim.getObject(f'/{robotName}/ikTarget')
 
+        self.simJoints = [self.sim.getObject(f'/{robotName}/joint{i + 1}') for i in range(6)]
+
         # Initialize the UR10 inverse kinematic
         self.ikEnv = self.simIK.createEnvironment()
         self.ikGroup = self.simIK.createGroup(self.ikEnv)
@@ -34,6 +38,16 @@ class UR10:
         ori = [ori[i] * 180 / math.pi for i in range(3)]
         return [pos[0],pos[1],pos[2],ori[0],ori[1],ori[2]]
 
+    def GetObjectMatrix(self, objectName):
+        handle = sim.getObject(f'/{objectName}')
+        # matrix: array of 12 values [Vx0 Vy0 Vz0 P0 Vx1 Vy1 Vz1 P1 Vx2 Vy2 Vz2 P2]
+        _matrix = sim.getObjectMatrix(handle, self.simrobot)
+        simMatrix = np.eye(4)
+        # convert the matrix to 4x4 matrix
+        for i in range(3):
+            simMatrix[i, :] = _matrix[i*4:i*4+4]
+        return simMatrix
+
     # Read the cartesion position of the robot
     def ReadPosition(self):
         pos = self.sim.getObjectPosition(self.simTip, self.simrobot)
@@ -42,6 +56,10 @@ class UR10:
         ori = [ori[i] * 180 / math.pi for i in range(3)]
         return [pos[0],pos[1],pos[2],ori[0],ori[1],ori[2]]
     
+    def ReadJointPosition(self):
+        jointPos = [self.sim.getJointPosition(joint) * 180 / math.pi for joint in self.simJoints]
+        return jointPos
+
     # Set robot position in mm and deg
     def SetPosition(self, targetPos):
         pos = [targetPos[i] / 1000 for i in range(3)]
@@ -52,6 +70,14 @@ class UR10:
         #self.simIK.handleGroup(self.ikEnv, self.ikGroup, {'syncWorlds' : True, 'allowError' : True})
         self.simIK.applyIkEnvironmentToScene(self.ikEnv, self.ikGroup)
     
+    def SetJointPosition(self, targetJointPos):
+        targetJointPos = [joint * math.pi / 180 for joint in targetJointPos]
+        for i in range(len(self.simJoints)):
+            self.sim.setJointPosition(self.simJoints[i], targetJointPos[i])
+        # Apply ik
+        #self.simIK.handleGroup(self.ikEnv, self.ikGroup, {'syncWorlds' : True, 'allowError' : True})
+        #self.simIK.applyIkEnvironmentToScene(self.ikEnv, self.ikGroup)
+
     def MoveL(self, targetPos, speed):
         initialPos = self.ReadPosition()
         interpolated_points, steps, time_per_step = linear_interpolation(initialPos, targetPos, speed, speed/20)
@@ -61,6 +87,14 @@ class UR10:
                 time_per_step = 0.05
             time.sleep(time_per_step)
     
+
+    def MoveJ(self, targetJointPos, speed):
+        initialJointPos = self.ReadJointPosition()
+        interpolated_points, steps, time_per_step = joint_interpolation(initialJointPos, targetJointPos, speed)
+        for point in interpolated_points:
+            self.SetJointPosition(point)
+            time.sleep(time_per_step)
+
     def MoveL_2(self, targetPos, speed):
         pass
 
@@ -72,14 +106,63 @@ if __name__ == '__main__':
         intialpos = armrobot.ReadPosition()
         targetPos = armrobot.GetObjectPosition('target1')
         targetPos2 = armrobot.GetObjectPosition('target2')
-        print(f'initial position: {intialpos}')
-        print(f'target position: {targetPos}')
         
-        while True:
-            armrobot.MoveL(targetPos, 300)
-            armrobot.MoveL(targetPos2, 200)
-            armrobot.MoveL(targetPos, 300)
-            armrobot.MoveL(targetPos2, 200)
+
+        # Move the robot in the joint space
+        #targetJoint = [0, 15, 90, 0, 0, 0]
+        #armrobot.MoveJ(targetJoint, 30 * math.pi / 180)
+
+
+        # ============================================================================== #
+        # Now the problem that we wanna solve is to define the workframe of the robot
+        user_frame = armrobot.GetObjectPosition('Cuboid/origin')
+        T_AB = pose2matrix(user_frame)
+
+
+        for i in range(10):
+            pos_from_user_frame = [0, 0, 0, 180, 0, 180]
+            T_BC = pose2matrix(pos_from_user_frame)
+            T_AC = np.dot(T_AB, T_BC)
+            endpos = getPosFromMatrix(T_AC)
+            armrobot.MoveL(endpos, 300)
+
+            pos_from_user_frame = [200, 0, 0, 180, 0, 90]
+            T_BC = pose2matrix(pos_from_user_frame)
+            T_AC = np.dot(T_AB, T_BC)
+            endpos = getPosFromMatrix(T_AC)
+            armrobot.MoveL(endpos, 300)
+
+            pos_from_user_frame = [200, 200, 0, 180, 0, 0]
+            T_BC = pose2matrix(pos_from_user_frame)
+            T_AC = np.dot(T_AB, T_BC)
+            endpos = getPosFromMatrix(T_AC)
+            armrobot.MoveL(endpos, 300)
+
+            pos_from_user_frame = [0, 200, 0, 180, 0, 0]
+            T_BC = pose2matrix(pos_from_user_frame)
+            T_AC = np.dot(T_AB, T_BC)
+            endpos = getPosFromMatrix(T_AC)
+            armrobot.MoveL(endpos, 300)
+        # ============================================================================== #
+        # simMatrix = armrobot.GetObjectMatrix('target1')
+        # print(f'target position: {targetPos}')
+
+        # print('sim calculation:')
+        # print(simMatrix)
+
+        # print('manual calculation:')
+        # simMatrix = pose2matrix(targetPos)
+        # print(simMatrix)
+
+        # rotAngle = matrix2rotAngle(simMatrix)
+        # eulerAngle = rot2euler(rotAngle)
+        # print(eulerAngle)
+
+        #while True:
+        #    armrobot.MoveL(targetPos, 300)
+        #    armrobot.MoveL(targetPos2, 200)
+        #    armrobot.MoveL(targetPos, 300)
+        #    armrobot.MoveL(targetPos2, 200)
         
         time.sleep(5)
         sim.stopSimulation()
--- 6_UR10/interpolation.py
+++ 6_UR10/interpolation.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 
 def linear_interpolation(intial_p, target_p, velocity, base_step_size=2):
     interpolated_points = []
-    
+    interpolated_points.append(intial_p)
+
     # Convert orientation from degrees to radians
     P1_orientation_rad = np.radians(intial_p[3:])
     P2_orientation_rad = np.radians(target_p[3:])
@@ -15,7 +16,7 @@ def linear_interpolation(intial_p, target_p, velocity, base_step_size=2):
     P2_quat = R.from_euler('xyz', P2_orientation_rad).as_quat()
 
     # Calculate the total distance
-    position_distance = np.linalg.norm(np.array(P2[:3]) - np.array(P1[:3]))
+    position_distance = np.linalg.norm(np.array(target_p[:3]) - np.array(intial_p[:3]))
     orientation_distance = R.from_quat(P1_quat).inv() * R.from_quat(P2_quat)
     orientation_distance = orientation_distance.magnitude()
     total_distance = position_distance + orientation_distance
@@ -26,20 +27,32 @@ def linear_interpolation(intial_p, target_p, velocity, base_step_size=2):
     # Calculate the number of steps based on the base step size
     steps = int(total_distance / base_step_size)
 
+    if steps <= 0:
+        steps = 1
+
     # Calculate the time per step
     time_per_step = total_time / steps
-
+    print(f'total_time: {time_per_step}')
+    
+    
     key_times = [0, 1]
     key_rots = R.from_quat([P1_quat, P2_quat])
     slerp = Slerp(key_times, key_rots)
     
     for i in range(steps + 1):
-        t = i / steps
+
+        # how to solve if steps = 0:
+        if steps == 0:
+            t = 1
+        else:
+            t = i / (steps)
+        
+        
         # Interpolate the position
         interpolated_pos = [intial_p[j] + t * (target_p[j] - intial_p[j]) for j in range(3)]
         interpolated_quat = slerp(t).as_quat()
 
-         # Convert the quaternion to Euler angles
+        # Convert the quaternion to Euler angles
         orientation_rad = R.from_quat(interpolated_quat).as_euler('xyz')
         orientation_deg = np.degrees(orientation_rad)
         # Append the interpolated point
@@ -47,6 +60,21 @@ def linear_interpolation(intial_p, target_p, velocity, base_step_size=2):
 
     return interpolated_points, steps, time_per_step
 
+def joint_interpolation(initial_joints, target_joints, speed):
+    # Calculate the number of steps based on the speed
+    max_joint_diff = max([abs(target_joints[i] - initial_joints[i]) for i in range(len(initial_joints))])
+    steps = int(max_joint_diff / speed * 100)  # Adjust the factor as needed for your speed unit
+    time_per_step = 1.0 / steps  # Assuming speed is in units per second
+
+    # Interpolate joint positions
+    interpolated_joints = []
+    for step in range(steps + 1):
+        interpolated_step = [
+            initial_joints[i] + (target_joints[i] - initial_joints[i]) * step / steps
+            for i in range(len(initial_joints))
+        ]
+        interpolated_joints.append(interpolated_step)
+    return interpolated_joints, steps, time_per_step    
 
 def plot_interpolated_points(interpolated_points):
     fig = plt.figure()
