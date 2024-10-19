4bc22f30463765e65fae0fcf09b71ea606795f74
yudarw <yudarismawahyudi@gmail.com>
2024-10-19 17:52:52 +0700

    added move linear function

--- 6_UR10/UR10_demo.py
+++ 6_UR10/UR10_demo.py
@@ -1,9 +1,12 @@
 from coppeliasim_zmqremoteapi_client import *
 import math
+import time
+from interpolation import linear_interpolation
 
 client = RemoteAPIClient()
 sim = client.require('sim')
 sim.startSimulation()
+
 
 class UR10:
     
@@ -41,13 +44,46 @@ class UR10:
         self.simIK.applyIkEnvironmentToScene(self.ikEnv, self.ikGroup)
     
     def MoveL(self, targetPos, speed):
+        initialPos = self.ReadPosition()
+        interpolated_points, steps, time_per_step = linear_interpolation(initialPos, targetPos, speed, speed/20)
+        for point in interpolated_points:
+            self.SetPosition(point)
+            
+            if (time_per_step < 0.05):
+                time_per_step = 0.05
+            time.sleep(time_per_step)
+    
+    def GetObjectPosition(self, objectName):
+        handle = sim.getObject(f'/{objectName}')
+        pos = self.sim.getObjectPosition(handle, self.simrobot)
+        ori = self.sim.getObjectOrientation(handle, self.simrobot)
+        pos = [pos[i] * 1000 for i in range(3)]
+        ori = [ori[i] * 180 / math.pi for i in range(3)]
+        return [pos[0],pos[1],pos[2],ori[0],ori[1],ori[2]]
+
+    def MoveL_2(self, targetPos, speed):
         pass
-        
+
 
 if __name__ == '__main__':
-        armrobot = UR10(client, 'UR10')
-        intialpos = armrobot.ReadPosition()
         
+        armrobot = UR10(client, 'UR3')
+
+        intialpos = armrobot.ReadPosition()
+        targetPos = armrobot.GetObjectPosition('target1')
+        targetPos2 = armrobot.GetObjectPosition('target2')
+        print(f'initial position: {intialpos}')
+        print(f'target position: {targetPos}')
+        
+        while True:
+            armrobot.MoveL(targetPos, 300)
+            armrobot.MoveL(targetPos2, 200)
+            armrobot.MoveL(targetPos, 300)
+            armrobot.MoveL(targetPos2, 200)
+        
+        time.sleep(5)
+        sim.stopSimulation()
+
         # Set position of x, y, z, rx, ry, rz
-        pos = [500, -200, 400, 180, 0, 0]
-        armrobot.SetPosition(pos)
+        #pos = [500, -200, 400, 180, 0, 0]
+        #armrobot.MoveL(pos)
---
+++ 6_UR10/interpolation.py
@@ -0,0 +1,102 @@
+import math
+import numpy as np
+from scipy.spatial.transform import Rotation as R, Slerp
+import matplotlib.pyplot as plt
+
+def linear_interpolation(P1, P2, velocity, base_step_size=2):
+    interpolated_points = []
+    
+    # Convert orientation from degrees to radians
+    P1_orientation_rad = np.radians(P1[3:])
+    P2_orientation_rad = np.radians(P2[3:])
+
+    # Convert Euler angles to quaternions
+    P1_quat = R.from_euler('xyz', P1_orientation_rad).as_quat()
+    P2_quat = R.from_euler('xyz', P2_orientation_rad).as_quat()
+
+    # Calculate the total distance
+    position_distance = np.linalg.norm(np.array(P2[:3]) - np.array(P1[:3]))
+    orientation_distance = R.from_quat(P1_quat).inv() * R.from_quat(P2_quat)
+    orientation_distance = orientation_distance.magnitude()
+    total_distance = position_distance + orientation_distance
+    
+    # Calculate the total time
+    total_time = total_distance / velocity
+
+    # Calculate the number of steps based on the base step size
+    steps = int(total_distance / base_step_size)
+
+    # Calculate the time per step
+    time_per_step = total_time / steps
+
+    key_times = [0, 1]
+    key_rots = R.from_quat([P1_quat, P2_quat])
+    slerp = Slerp(key_times, key_rots)
+    
+    for i in range(steps + 1):
+        t = i / steps
+
+        # Interpolate the position
+        interpolated_pos = [P1[j] + t * (P2[j] - P1[j]) for j in range(3)]
+        
+        interpolated_quat = slerp(t).as_quat()
+
+         # Convert the quaternion to Euler angles
+        orientation_rad = R.from_quat(interpolated_quat).as_euler('xyz')
+        orientation_deg = np.degrees(orientation_rad)
+        
+        # Append the interpolated point
+        interpolated_points.append([interpolated_pos[0], interpolated_pos[1], interpolated_pos[2], orientation_deg[0], orientation_deg[1], orientation_deg[2]])
+
+    return interpolated_points, steps, time_per_step
+
+
+def plot_interpolated_points(interpolated_points):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Extract position and orientation data
+    x = [point[0] for point in interpolated_points]
+    y = [point[1] for point in interpolated_points]
+    z = [point[2] for point in interpolated_points]
+    orientations = [point[3:] for point in interpolated_points]
+
+    # Plot the interpolated points
+    ax.plot(x, y, z, label='Interpolated Path')
+
+    # Plot the orientation frames
+    for i in range(len(interpolated_points)):
+        pos = np.array([x[i], y[i], z[i]])
+        orientation_rad = np.radians(orientations[i])
+        rot = R.from_euler('xyz', orientation_rad)
+        frame = rot.as_matrix()
+
+        # Plot the orientation frame
+        ax.quiver(pos[0], pos[1], pos[2], frame[0, 0], frame[0, 1], frame[0, 2], color='r', length=10)
+        ax.quiver(pos[0], pos[1], pos[2], frame[1, 0], frame[1, 1], frame[1, 2], color='g', length=10)
+        ax.quiver(pos[0], pos[1], pos[2], frame[2, 0], frame[2, 1], frame[2, 2], color='b', length=10)
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.legend()
+    plt.show()
+
+def test_linear_interpolation():
+    P1 = [500, -200, 400, 180, 0, 0]
+    P2 = [600, -200, 400, 90, 0, 0]
+    velocity = 10
+    interpolated_points, steps, time_per_step = linear_interpolation(P1, P2, velocity)
+    #plot_interpolated_points(interpolated_points)
+    print(f'velocity: {velocity}')  
+    print(f'steps: {steps}')
+    print(f'time_per_step: {time_per_step}')
+    
+    #print(interpolated_points)
+
+    # Create a graph that showing the interpolated points as well as the orientation frame using matplotlib
+
+
+if __name__ == '__main__':
+    test_linear_interpolation()
+
