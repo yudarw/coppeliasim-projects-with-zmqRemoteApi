6c020a40d7334784e28f406a616055d2b4a3f66a
yudarw <yudarismawahyudi@gmail.com>
2024-10-23 11:10:17 +0700

    add getmapping and getdistance function

--- source/sick_tim310.py
+++ source/sick_tim310.py
@@ -1,39 +1,62 @@
-import sys
-import os
-sys.path.append(os.getcwd())
 from coppeliasim_zmqremoteapi_client import *
-from lib.MobileRobot import MobileRobot
 import threading
 import time
 import struct
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 class SickTIM310:
 
     def __init__(self, sensor_name):
-        client = RemoteAPIClient()
-        self.sim = client.require('sim')
-        # Get the sensor and script handle
-        self.sensor = self.sim.getObject(f'{sensor_name}')
-        self.script_handle = self.sim.getScript('')
+        try:
+            self.client = RemoteAPIClient()
+            self.sim = self.client.require('sim')
+            # Get the sensor and script handle
+            self.sensor = self.sim.getObject(f'{sensor_name}')
+            self.script_handle = self.sim.getScript(sim.scripttype_childscript,self.sensor)
+            self.lidar_data = []
 
+        except Exception as e:
+            print('Error: ', e)
+            self.client.__del__()    
 
+    def getMapping(self):
+        
+        self.lidar_data = self.sim.callScriptFunction('get_data', self.script_handle)
+        x = [self.lidar_data[3 * i] for i in range(270)]
+        y = [self.lidar_data[3 * i + 1] for i in range(270)]
+        return x, y
 
+    def  getDistance(self, angle):
 
-
-
-
-
-
-
-
-
+        # Get the data from the sensor:
+        self.lidar_data = self.sim.callScriptFunction('get_data', self.script_handle)
+        x = self.lidar_data[3 * angle]
+        y = self.lidar_data[3 * angle + 1]
+        
+        # Calulate the norm of the vector to get the distance:
+        distance = np.linalg.norm([x, y]) * 100
+        return distance
+    
+    def getAllDistances(self):
+        distances = []
+        self.lidar_data = self.sim.callScriptFunction('get_data', self.script_handle)
+        for i in range(270):
+            x = self.lidar_data[3 * i]
+            y = self.lidar_data[3 * i + 1]
+            distance = np.linalg.norm([x, y]) * 100
+            distances.append(distance)
+            print('Angle: ', i, 'Distance: ', distance)
+        return distances
 
 
 if __name__ == '__main__':
-    pass
+    client = RemoteAPIClient()
+    sim = client.require('sim')
+    sim.startSimulation()
+
+    sensor = SickTIM310('./SickTIM310')
+    sensor.getAllDistances()
 
 
 
