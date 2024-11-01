9c5e2c1e4082cc76aa466a74a4d99d5289fd1e4a
yudarw <yudarismawahyudi@gmail.com>
2024-11-01 23:50:51 +0700

    Add pick and place methods

--- 7_pick_and_place/ur10_pick_and_place_demo.py
+++ 7_pick_and_place/ur10_pick_and_place_demo.py
@@ -10,51 +10,45 @@ import csv
 client = RemoteAPIClient()
 sim = client.require('sim')
 armRobot = UniversalRobot('UR10')
+armRobot.AttachGripper('vacuum_gripper')
 
-def SavePalletPositiion():
-    with open('pallet_positions.csv', mode='w') as file:
+
+
+def SavePalletPosition():
+    with open('pallet_positions.csv', mode='w',newline='') as file:
         writer = csv.writer(file)
         for i in range(21):
             pos = armRobot.GetObjectPosition(f'Cartoons{i + 1}')
-            print(f"position of Cartoons{i + 1}: {pos}")
             writer.writerow(pos)
+
+def LoadPalletPosition():
+    positions = []
+    with open('pallet_positions.csv', mode='r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            positions.append([float(i) for i in row])
+    return positions
+
 
 def main():
     sim.startSimulation()
-
-    
-    armRobot.AttachGripper('vacuum_gripper')
-    armRobot.gripper.Catch()
-    
-    # Sensor handle:
+    # Get proximity sensor handle to detect the incoming object
     prox_sensor = sim.getObject('/ConveyorSensor')
 
+    # Setup the pickup position of the object in the conveyor
+    pickUpPos = [570, 140, 87, 180, 0, 90]
+    liftUpPos = [570, 140, 200, 180, 0, 90]
+    
+    # Complete the pallet collection
+    count = 0
     while True:
         ret = sim.readProximitySensor(prox_sensor)
         if ret[0] == 1:
-
-            # PickUp Position
-            pickUpPos = [570, 140, 90, 180, 0, 90]
-            armRobot.MoveL(pickUpPos,50)
-
-            time.sleep(0.5)
-
-            liftUpPos = [570, 140, 200, 180, 0, 90]
-            armRobot.MoveL(liftUpPos,50)
-
-            time.sleep(1)
-
-            armRobot.gripper.Release()
-
-            time.sleep(1)
-            break
-
+            pass
         time.sleep(0.1)
 
     sim.stopSimulation()
 
 if __name__ == '__main__':
-    #main()
-    sim.startSimulation()
-    SavePalletPositiion()
+    main()
 
