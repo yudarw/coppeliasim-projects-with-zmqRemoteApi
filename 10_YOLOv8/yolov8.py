0952df4ae9ecea83d27bc7d040f04d519c13ff3d
yudarw <yudarismawahyudi@gmail.com>
2024-11-05 09:29:26 +0700

    Added yolov8 example

---
+++ 10_YOLOv8/yolov8.py
@@ -0,0 +1,23 @@
+from ultralytics import YOLO
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+model = YOLO('yolov8n.pt')
+cap = cv.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+        
+    results = model.predict(frame)
+    #qresults.show()
+    image = results[0].plot()
+
+    image_bgr = cv.cvtColor(image, cv.COLOR_RGB2BGR)
+    cv.imshow('results', image)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
